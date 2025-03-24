bae35763e67f638326164af7a8d1fcdbaa3b4664
Oscar Renalias <oscar@renalias.net>
2025-03-24 10:37:30 +0000

    Added support for logging

--- agents/receiptanalyzer.py
+++ agents/receiptanalyzer.py
@@ -1,11 +1,12 @@
 from .receiptstate import ReceiptState
+import logging
 
 class ReceiptAnalyzer:
     """
         Analyzes purchases receipts
     """
     def __init__(self):
-        pass
+        logging.info("ReceiptAnalyzer initialized")
 
     def run(self, state: ReceiptState) -> ReceiptState:
         return(state)
