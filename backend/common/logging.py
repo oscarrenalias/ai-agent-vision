78ab9164e46ccacc26b3424f1161c20ac2db7c7d
Renalias, Oscar <oscar@renalias.net>
2025-04-29 21:17:54 +0000

    Added support for providing a default logging level for our internal classes.

--- backend/common/logging.py
+++ backend/common/logging.py
@@ -1,7 +1,7 @@
 import logging
 
 
-def configure_logging():
+def configure_logging(default_log_level=logging.INFO):
     logging.basicConfig(
         level=logging.INFO, format="%(asctime)s | %(levelname)-8s | %(module)s:%(funcName)s:%(lineno)d - %(message)s"
     )
@@ -10,7 +10,7 @@ def configure_logging():
     info_loggers = ["agents", "common"]
     for logger_name in info_loggers:
         logger = logging.getLogger(logger_name)
-        logger.setLevel(logging.INFO)
+        logger.setLevel(default_log_level)
 
     # This pesky logger is used by langchain and we don't want to see its output
     logging.getLogger("httpx").disabled = True
